44202e31532442e9c2b29d9e4a842b788ac1b940
Elze Hamilton <elze.hamilton@gmail.com>
2021-02-09 10:24:38 -0600

    Added a comment to indicate that a certain method call should not be used in real-life application, and is only for demonstration purposes

--- client/src/utils.js
+++ client/src/utils.js
@@ -15,6 +15,11 @@ const utils = {
 		convertedForecast = {...forecast, temperature: temperature.toFixed()};
 		//console.log(`^^^^^^^^^^^^^^ utils.getForecastInCurrentUnits ; tempUnit = ${tempUnit} returning convertedForecast = ${JSON.stringify(convertedForecast)}  `);
 	}		
+	
+	// Do not call PUT here, this is only for the purpose to demonstrate a spy on axios.put in unit tests. 
+	// This is not a good practice, because (a) reducer functions should not perform side effects, 
+	// and (b) axios.put should be called with "await", but that would require making this function async, 
+	// and it would make it tricky to call it from a reducer
 	axios.put(`/api/tempUnit`, { temperatureUnit: tempUnit });
 	return convertedForecast;	
   }
